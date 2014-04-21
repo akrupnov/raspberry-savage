05891669c67604276eedcd81b725469fb4f76378
Alex Krupnov <akrupnov@gmail.com>
2014-04-21 17:08:33 +0200

    Generated project from archetype

---
+++ src/main/java/com/akrupnov/fun/App.java
@@ -0,0 +1,13 @@
+package com.akrupnov.fun;
+
+/**
+ * Sandbox app!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Pi!" );
+    }
+}
